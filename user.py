2b267b4ca255243b8d61057114345de6158ed308
lebowski34 <167763740+lebowski34@users.noreply.github.com>
2024-09-10 14:06:28 +0300

    Update user.py

--- user.py
+++ user.py
@@ -1,15 +1,19 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.orm import relationship
 from app.backend.db import Base
 
-class User(Base):
-    __tablename__ = 'users'
+class Task(Base):
+    __tablename__ = 'tasks'
 
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String)
-    firstname = Column(String)
-    lastname = Column(String)
-    age = Column(Integer)
+    title = Column(String, index=True)
+    content = Column(String)
+    priority = Column(Integer, default=0)
+    completed = Column(Boolean, default=False)
+    user_id = Column(Integer, ForeignKey('tasks.id'), nullable=False, index=True)
     slug = Column(String, unique=True, index=True)
 
-    tasks = relationship("Task", back_populates="user")
+    user = relationship("User", back_populates="tasks")
+
+from sqlalchemy.schema import CreateTable
+print(CreateTable(Task.__table__))
