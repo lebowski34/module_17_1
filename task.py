4230e11b80111593213d7adaec3dc5533bda46d8
lebowski34 <167763740+lebowski34@users.noreply.github.com>
2024-09-10 14:05:47 +0300

    Update task.py

--- task.py
+++ task.py
@@ -5,12 +5,12 @@ from app.backend.db import Base
 class Task(Base):
     __tablename__ = 'tasks'
 
-    id = Column(Integer, primary_key=True, index=Traue)
+    id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True)
     content = Column(String)
     priority = Column(Integer, default=0)
     completed = Column(Boolean, default=False)
-    user_id = Column(Integer, ForeignKey('users.id'), nullable=False, index=True)
+    user_id = Column(Integer, ForeignKey('tasks.id'), nullable=False, index=True)
     slug = Column(String, unique=True, index=True)
 
     user = relationship("User", back_populates="tasks")
